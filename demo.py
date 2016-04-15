dfc7d96aef62a8b02698121bfa352263559fc6b9
Madeleine Hardt <hardtmad@grinnell.edu>
2016-04-15 17:23:58 +0000

    improved format for demo

--- demo.py
+++ demo.py
@@ -2,10 +2,29 @@ __author__ = 'loaner'
 from addDoc import trainingCorpus
 from keywordLearner import solutionTrainer
 from SpellerCorrector import Corrector
-
+  
 
 if __name__ == '__main__':
+   # x = ('dog',3)
+   # y = (4,6)
+   # z = (7,)
 
+    ##list = []
+    #list.append(x)
+    #list.append(y)
+    #list.append(z)
+    z = 'dog and cat'
+    g = (z,)
+    t = {g : 0}
+    t = "a"
+#    z = list['dog']
+  #  CS = trainingCorpus()
+  #  FQ = trainingCorpus()
+  ##  QF = trainingCorpus()
+   # SR = trainingCorpus()
+   # EM = trainingCorpus()
+   # SM = trainingCorpus()
+   # BU = trainingCorpus()
 
     TC = trainingCorpus()
     TC.addTopic('complete square')
@@ -25,55 +44,22 @@ if __name__ == '__main__':
     TC.addDoc('Help.txt', 'help')
 
 
+
+ #   sentencesCS = CS.createSentances()
+ #   sentencesFQ = FQ.createSentances()
+ #   sentencesQF = QF.createSentances()
+ #   sentencesSR = SR.createSentances()
+ #   sentencesSM = SM.createSentances()
+ #   sentencesEM = EM.createSentances()
+ #   sentencesBU = BU.createSentances()
+
+
+
+    #stuff = solutionTrainer(sentencesCS + sentencesFQ + sentencesQF + sentencesSR + sentencesBU + sentencesBU
+    #                        + sentencesBU, .3)
+
     model = solutionTrainer(TC, .3)
 
-<<<<<<< HEAD
-
-    #aa = model.predictMethod("I want to do the quadratic formula")
-    #ab = model.predictMethod("I want to do the qudratic formula")
-    ac = model.predictMethod("I want to do the quadrtic formula")
-    help = model.predictMethod("help me i don't know what to do")
-    ad = model.predictMethod("I want to do the queadrtic formula")
-    ae = model.predictMethod(("I want to do the quadratic formula, not factor the quadratic"))
-    af = model.predictMethod('lets do the square root')
-
-
-
-    ag = model.predictMethod('complete-the-squares method')
-    ah = model.predictMethod('complete-the-square method')
-    #ac = stuff.predictMethod('complete the squares method')
-    #ad = stuff.predictMethod('complete the square method')
-
-
-    #ae = stuff.predictMethod("i want to replace stuff in the equation")
-    #bf = model.predictMethod("i want to break apart the equation")
-    #ag = stuff.predictMethod('lets do the square root')
-    #ah = stuff.predictMethod('i want to square it')
-    #ai = model.predictMethod('use the complete the squares method')
-    #aj = model.predictMethod('i\'ll use the method of completing the squares')
-    #ak = model.predictMethod('komplete da skware')
-    #al = model.predictMethod('i\'m not sure')
-    #am = stuff.predictMethod('complete-the-squares method')
-    #an = stuff.predictMethod('i\'d use the quadratic formula cuz itz my fave')
-    ##ao = stuff.predictMethod('dunno, maybe quadratic?')
-    #ap = model.predictMethod('i\'ll factor quadratics')
-    #aq = stuff.predictMethod('i plan to complete the squares')
-    ##ar = stuff.predictMethod('do a square root')
-    #at = model.predictMethod('i have no idea')
-    #au = model.predictMethod('help')
-    #av = stuff.predictMethod('Xyzzy')
-    #aw = stuff.predictMethod('Use Kolmolgorov Turbulence')
-    #ax = stuff.predictMethod('Factor Third-order Partial Differential Equations')
-    #ay = model.predictMethod('Consult the i ching')
-    #z23 = stuff.predictMethod('Read Tea Leaves')
-
-    #z8 = stuff.predictMethod('i\'ll use the method of completing the squares')
-    #z8 = stuff.predictMethod('i\'ll use the method of completing the squares')
-    ##z8 = stuff.predictMethod('i\'ll use the method of completing the squares')
-    #z8 = stuff.predictMethod('i\'ll use the method of completing the squares')
-
-
-=======
     #stuff.createModel(sentencesCS, 'complete square')
     #stuff.createModel(sentencesFQ, 'factor quadratic')
     #stuff.createModel(sentencesQF, 'quadratic formula')
@@ -239,7 +225,6 @@ if __name__ == '__main__':
     
     # User input examples
     
->>>>>>> cc1eef74d43f791a705d559abc78f8f077e7b04b
 
 
 
